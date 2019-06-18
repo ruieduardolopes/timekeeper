a92a9dd56ce7f5b13f4c578b13fdfa9475ec1f77
Rui Lopes <ruieduardo.fa.lopes@av.it.pt>
2019-06-18 20:58:26 +0100

    [WIP] Partial completion of server-client communication in cmd/timekeeper

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,6 +1,6 @@
 use chrono::offset::TimeZone;
 use chrono::Utc;
-use std::io::{Error, Write};
+use std::io::{Error, Write, Read};
 use std::net::TcpStream;
 
 pub fn handle_client(mut stream: TcpStream) -> Result<(), Error> {
@@ -14,6 +14,20 @@ pub fn handle_client(mut stream: TcpStream) -> Result<(), Error> {
     ];
 
     stream.write(time_slice)?;
+
+    let mut offset_slice_from_client: [u8; 4] = [0, 0, 0, 0];
+    stream.read(&mut offset_slice_from_client)?;
+
+    // TODO estimate all offsets and retrieve a correction
+
+    let correction: i64 = 0;
+    let correction_slice = &[
+        ((correction >> 24) & 0xFF) as u8,
+        ((correction >> 16) & 0xFF) as u8,
+        ((correction >> 8) & 0xFF) as u8,
+        ((correction >> 0) & 0xFF) as u8,
+    ];
+    stream.write(correction_slice);
 
     Ok(())
 }
--- src/subcommands/update.rs
+++ src/subcommands/update.rs
@@ -1,5 +1,5 @@
 use chrono::Utc;
-use std::io::{Error, Read};
+use std::io::{Error, Read, Write};
 use std::net::{Ipv4Addr, TcpStream};
 
 pub fn init(address: Ipv4Addr, port: u16) -> Result<(), Error> {
@@ -8,12 +8,31 @@ pub fn init(address: Ipv4Addr, port: u16) -> Result<(), Error> {
     let mut time_slice_from_server: [u8; 4] = [0, 0, 0, 0];
     stream.read(&mut time_slice_from_server)?;
 
+    let current_time = Utc::now().timestamp();
+
     let time_from_server = ((time_slice_from_server[0] as i64) << 24)
         + ((time_slice_from_server[1] as i64) << 16)
         + (((time_slice_from_server[2]) as i64) << 8)
         + ((time_slice_from_server[3]) as i64);
 
-    let current_time = Utc::now().timestamp();
+    let time_difference = current_time - time_from_server;
+
+    let difference_slice = &[
+        ((time_difference >> 24) & 0xFF) as u8,
+        ((time_difference >> 16) & 0xFF) as u8,
+        ((time_difference >> 8) & 0xFF) as u8,
+        ((time_difference >> 0) & 0xFF) as u8,
+    ];
+    stream.write(difference_slice)?;
+
+    let mut time_correction: [u8; 4] = [0, 0, 0, 0];
+    stream.read(&mut time_correction)?;
+    let correction = ((time_correction[0] as i64) << 24)
+        + ((time_correction[1] as i64) << 16)
+        + (((time_correction[2]) as i64) << 8)
+        + ((time_correction[3]) as i64);
+
+    // TODO adjust this clock by correction
 
     Ok(())
 }

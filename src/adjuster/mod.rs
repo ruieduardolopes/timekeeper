843aefee9155f658a3f81b1df797f41693ad53a5
Rui Lopes <ruieduardo.fa.lopes@av.it.pt>
2019-06-25 19:03:02 +0100

    Change settimeofday to clock_settime in cmd/timekeeper, due to incompatibilities

--- src/adjuster/mod.rs
+++ src/adjuster/mod.rs
@@ -1,15 +1,17 @@
 use std::io::Error;
 use std::process::Command;
 use time::Duration;
+use libc::*;
 
 pub fn set_time_by_offset(offset: Duration) -> Result<(), Error> {
     if cfg!(target_os = "linux") {
         let new = time::now().to_timespec() + offset;
-        let timeval = libc::timeval {
-            tv_sec: new.sec,
-            tv_usec: (new.nsec as f64 * 0.001) as i64,
+        let timespec = libc::timespec {
+            // TODO: fix this conditionally given an arch...
+            tv_sec: new.sec ,
+            tv_nsec: new.nsec as i64,
         };
-        unsafe { libc::settimeofday(&timeval, std::ptr::null()) };
+        unsafe { libc::clock_settime(libc::CLOCK_REALTIME, &timespec) };
     } else {
         panic!("Operative System or Architecture not supported.");
     }
